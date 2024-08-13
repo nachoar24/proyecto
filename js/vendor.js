6d29c0df6479010d4b657e0fec9e83289011d5dc
nachoar24 <nacho12ar@gmail.com>
2024-08-12 21:00:07 -0600

    vendor.js

---
+++ js/vendor.js
@@ -0,0 +1,9 @@
+// jQuery
+/*! jQuery v3.6.0 | (c) OpenJS Foundation and other contributors | jquery.org/license */
+!function(a,b){"use strict"; /* jQuery library code */ }(window);
+
+// Bootstrap
+/*! Bootstrap v5.3.0 | (c) Bootstrap | MIT License */
+!function(a,b){"use strict"; /* Bootstrap library code */ }(window);
+
+// Other third-party libraries
